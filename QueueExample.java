20d7483c26c05c3faf6b413ed7f78bc403ac4e71
rajeshwarjagtap <97775246+rajeshwarjagtap@users.noreply.github.com>
2022-01-24 02:34:30 +0530

    Add files via upload

---
+++ QueueExample.java
@@ -0,0 +1,27 @@
+package first2A;
+
+import java.util.*;
+
+public class QueueExample{
+
+
+public static void main(String[] args) {
+
+Queue<String>locationsQueue =(Queue<String>) new LinkedList();
+locationsQueue.add("Kolkata");
+locationsQueue.add("Patna"); 
+locationsQueue.add("Delhi");
+
+locationsQueue.add("Gurgaon");
+
+locationsQueue.add("Noida");
+
+System.out.println(" Queue is :"+ locationsQueue);
+
+System.out.println("Head of Queue:"+ locationsQueue.peek());
+System.out.println(" After removing Head of Queue:"+locationsQueue);
+
+
+System.out.println("Size of Queue: "+locationsQueue.size());
+}
+}
